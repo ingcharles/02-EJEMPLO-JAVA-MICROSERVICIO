6b472fade373c65889fb7b92f53c5454680d2003
Carlos Anchundia <e.du.ingcharles@hotmail.com>
2024-04-29 21:44:59 -0500

    microservicio base final

--- erp-catalogo-dataaccess-service/src/main/java/ec/gob/imark/erp/catalogo/entity/LocalizacionEntity.java
+++ erp-catalogo-dataaccess-service/src/main/java/ec/gob/imark/erp/catalogo/entity/LocalizacionEntity.java
@@ -1,38 +1,13 @@
-/**
- * <p> Proyecto erp-catalogo.
- * <p> Clase LocalizacionEntity 24/4/2024.
- * <p> Copyright 2024 Consejo de la Judicatura.
- * <p> Todos los derechos reservados.
- */
 package ec.gob.imark.erp.catalogo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.Comment;
 
-/**
- * -- AQUI AÑADIR LA DESCRIPCION DE LA CLASE --.
- *
- * <p>Historial de cambios:
- *
- * <ul>
- *   <li>1.0.0 - Descripción del cambio inicial - Carlos.Anchundia - 24/4/2024
- *       <!-- Añadir nuevas entradas de cambios aquí -->
- * </ul>
- *
- * @author Carlos.Anchundia
- * @version 1.0.0 $
- * @since 24/4/2024
- */
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -42,45 +17,44 @@ import lombok.Setter;
 @Table(name = "localizacion", schema = "esq_catalogo")
 @Entity
 public class LocalizacionEntity {
+    @Id
+    @Size(max = 100)
+    @Column(name = "id_localizacion", nullable = false, length = 100)
+    private String idLocalizacion;
 
-  @Id
-  @NotBlank(message = "{localizacion.idLocalizacion.not_blank}")
-  @Column(name = "id_localizacion", nullable = false)
-  private String idLocalizacion;
+    @NotBlank(message = "{localizacion.nombreLocalizacion.not_blank}")
+    @Size(max = 100, message = "{localizacion.nombreLocalizacion.size}")
+    @Comment("AAAAA")
+    @Column(name = "nombre_localizacion", length = 100)
+    private String nombreLocalizacion;
 
-  @NotBlank(message = "{localizacion.nombreLocalizacion.not_blank}")
-  @Size(max = 100, message = "{localizacion.nombreLocalizacion.size}")
-  @Column(name = "nombre_localizacion", length = 100)
-  private String nombreLocalizacion;
+    @Size(max = 100)
+    @Column(name = "codigo_localizacion", length = 100)
+    private String codigoLocalizacion;
 
-  @NotBlank(message = "{localizacion.nivelLocalizacion.not_blank}")
-  @Column(name = "nivel_localizacion", nullable = false)
-  private Integer nivelLocalizacion;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_localizacion_padre")
+    private LocalizacionEntity idLocalizacionPadre;
+
+    @Column(name = "es_urbana_localizacion")
+    private Integer esUrbanaLocalizacion;
+
+    @Column(name = "nivel_localizacion")
+    private Integer nivelLocalizacion;
+
+    @Size(max = 100)
+    @Column(name = "nombre_nacionalidad_localizacion", length = 100)
+    private String nombreNacionalidadLocalizacion;
+
+    @Column(name = "tiene_juzgado_localizacion")
+    private Integer tieneJuzgadoLocalizacion;
+    @OneToMany(mappedBy = "idLocalizacionPadre")
+    private Set<LocalizacionEntity> localizacions = new LinkedHashSet<>();
+
 /*
-  @NotBlank(message = "{TipoAccion.nivelLocalizacion.not_blank}")
-  @Column(name = "IdMateria", nullable = false)
-  private Integer nivelLocalizacion;
-
-  @NotNull(message = "TipoAccion.estado.not_null")
-  @Size(max = 1, message = "{TipoAccion.estado.size}")
-  @Enumerated(EnumType.STRING)
-  @Column(name = "Estado", length = 1)
-  private EstadoEnum estado;*/
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(idLocalizacion);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    LocalizacionEntity that = (LocalizacionEntity) o;
-    return idLocalizacion.equals(that.idLocalizacion);
-  }
+ TODO [Reverse Engineering] create field to map the 'regimen_escolar_localizacion' column
+ Available actions: Define target Java type | Uncomment as is | Remove column mapping
+    @Column(name = "regimen_escolar_localizacion", columnDefinition = ""char"")
+    private Object regimenEscolarLocalizacion;
+*/
 }
