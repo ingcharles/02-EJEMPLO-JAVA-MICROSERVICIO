2f97c81417bee726f153e1417e277ac4c9864173
Carlos Anchundia <carlos.anchundia@funcionjudicial.gob.ecgit config --global user.email carlos.anchundia@funcionjudicial.gob.ec>
2024-05-30 15:19:35 -0500

    ejemplo basico sin guarda y sin actualizar

--- expel-catalogo-dataaccess-service/src/main/java/ec/gob/funcionjudicial/expel/catalogo/juridico/enumeration/NemonicoEnum.java
+++
@@ -1,8 +0,0 @@
-package ec.gob.funcionjudicial.expel.catalogo.juridico.enumeration;
-
-import lombok.Getter;
-
-@Getter
-public enum NemonicoEnum {
-  ANTERIOR, COIP, COGEP, COA
-}
--- expel-catalogo-domain-service/expel-catalogo-domain-application-service/src/main/java/ec/gob/funcionjudicial/expel/dataaccess/ports/input/MateriaApplicationService.java
+++
@@ -1,9 +0,0 @@
-package ec.gob.funcionjudicial.expel.dataaccess.ports.input;
-
-import ec.gob.funcionjudicial.expel.catalogo.to.juridico.MateriaTo;
-import java.util.List;
-
-public interface MateriaApplicationService {
-
-  List<MateriaTo> getActiveMaterias();
-}
